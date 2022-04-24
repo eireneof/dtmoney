3d1d5391b9d006b5fe07d657c9402681d38817e7
eireneof <eof@ic.ufal.br>
2022-04-24 11:19:00 -0300

    feat: mudando cor de seleção de acordo com o tipo de transação
    adição do atributo activeColor para mudar a cor para verde se for um depósito ou vermelho se or uma saída. Uso da função tranparentize para dar tranparência ao background color do botão.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -34,22 +34,26 @@ export function NewTransactionModal({
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
         <TransactionTypeContainer>
+
           <RadioBox
             type="button"
             onClick={() => {
               setType("deposit");
             }}
             isActive={type === "deposit"}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
           </RadioBox>
+
           <RadioBox
             type="button"
             onClick={() => {
               setType("withdraw");
             }}
             isActive={type === "withdraw"}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
