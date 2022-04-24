ca1c9292d3d847f693a79e95c63d4a4bc704acdc
eireneof <eof@ic.ufal.br>
2022-04-24 14:08:43 -0300

    fix: mudando chamada de variável de cor --text-body-color para --text-body
    --text-body-color não faz parte das variáveis de cor da aplicação, provavelmente foi uma sugestão do tabnine que foi aceita. Por isso, os textos das tags th estavam com uma coloração diferente da pretendida.

--- src/components/TrasactionsTable/styles.ts
+++ src/components/TrasactionsTable/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.div`
         border-spacing: 0 0.5rem;
 
         th { 
-            color: var(--text-body-color);
+            color: var(--text-body);
             font-weight: 400;
             padding: 1rem 2rem;
             text-align: left;
