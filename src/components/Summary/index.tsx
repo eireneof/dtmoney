9aa2c714f9ce8e4c7211191619ee475ba4f2d3b2
eireneof <eof@ic.ufal.br>
2022-04-24 15:08:11 -0300

    refactor: trocando chamada da api para o uso de contexto (TransactionsContext)

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,38 @@
-import incomeImg from '../../assets/entradas.svg'
-import outcomeImg from '../../assets/saidas.svg'
-import totalImg from '../../assets/total.svg'
+import { useContext } from "react";
+import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saidas.svg";
+import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContexts";
 
 import { Container } from "./styles";
 
 export function Summary() {
-    return(
-        <Container>
-            <div>
-                <header>
-                    <p>Entradas</p>
-                    <img src={incomeImg} alt="seta para cima indicando entradas"/>
-                </header>
-                <strong>R$ 1000,00</strong>
-            </div>
+    const transactions = useContext(TransactionsContext);
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="seta para cima indicando entradas" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </div>
 
-            <div>
-                <header>
-                    <p>Saídas</p>
-                    <img src={outcomeImg} alt="seta para baixo indicando saídas"/>
-                </header>
-                <strong>-R$ 500,00</strong>
-            </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="seta para baixo indicando saídas" />
+        </header>
+        <strong>-R$ 500,00</strong>
+      </div>
 
-            <div className="highlight-background">
-                <header>
-                    <p>Total</p>
-                    <img src={totalImg} alt="ícone de cifrão representando dinheiro"/>
-                </header>
-                <strong>+R$ 500,00</strong>
-            </div>
-        </Container>
-    );
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="ícone de cifrão representando dinheiro" />
+        </header>
+        <strong>+R$ 500,00</strong>
+      </div>
+    </Container>
+  );
 }
