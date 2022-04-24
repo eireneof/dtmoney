c3bfaf9e6e404afb556b4261c3e5f3ec05212d72
eireneof <eof@ic.ufal.br>
2022-04-24 07:42:03 -0300

    feat: adiconando form para inserção de novas transações ao modal de NewTransactionModal
    adição apenas do html, sem estilização, com um título, tags de input e botão

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,29 @@
-import Modal from 'react-modal';
+import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
-    isOpen: boolean;
-    onRequestClose: () => void;
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-    return(
-        <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-      >
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
         <h2>Cadastar Transação</h2>
-      </Modal>
-    );
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit"> Cadastrar </button>
+      </Container>
+    </Modal>
+  );
 }
